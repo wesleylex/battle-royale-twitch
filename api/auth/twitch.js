05432c2135a42cdca11f5f7dafb71dac8b0004a6
Wesley <wesleylex@gmail.com>
2025-12-26 18:28:10 -0300

    novos aquivos

--- api/auth/twitch.js
+++ api/auth/twitch.js
@@ -1,5 +1,39 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    const authUrl =
+      "https://id.twitch.tv/oauth2/authorize" +
+      `?client_id=${process.env.TWITCH_CLIENT_ID}` +
+      `&redirect_uri=${process.env.TWITCH_REDIRECT}` +
+      "&response_type=code" +
+      "&scope=chat:read chat:edit";
+
+    return res.redirect(authUrl);
+  }
+
+  const tokenRes = await axios.post(
+    "https://id.twitch.tv/oauth2/token",
+    null,
+    {
+      params: {
+        client_id: process.env.TWITCH_CLIENT_ID,
+        client_secret: process.env.TWITCH_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: process.env.TWITCH_REDIRECT
+      }
+    }
+  );
+
+  const { access_token } = tokenRes.data;
+
+  res.setHeader(
+    "Set-Cookie",
+    `twitch_token=${access_token}; Path=/; HttpOnly; Secure; SameSite=Lax`
+  );
+
   res.redirect("/");
 }
--- api/twitch/connect.js
+++ api/twitch/connect.js
@@ -1,3 +1,16 @@
-export default function handler(req, res) {
+import { connectTwitch } from "../../lib/twitchClient.js";
+
+export default async function handler(req, res) {
+  const cookie = req.headers.cookie || "";
+  const token = cookie
+    .split("; ")
+    .find(c => c.startsWith("twitch_token="))
+    ?.split("=")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Não autenticado" });
+  }
+
+  await connectTwitch(token);
   res.json({ ok: true });
 }
